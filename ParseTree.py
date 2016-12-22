c8a81447220ec3680059babca64dbb4414f1f5f2
Zhen Zhang <zz.albert@gmail.com>
2016-12-22 14:20:42 -0800

    Configure Pycharm and GiHub

---
+++ ParseTree.py
@@ -0,0 +1,49 @@
+# practise the stack function
+from pythonds.basic.stack import Stack
+from pythonds.trees.binaryTree import BinaryTree
+
+
+def buildParseTree(fpexp):
+    fplist = fpexp.split()
+    pStack = Stack()
+    eTree = BinaryTree('')
+    pStack.push(eTree)
+    currentTree = eTree
+    for i in fplist:
+        if i == '(':
+            currentTree.insertLeft('')
+            # why
+            pStack.push(currentTree)
+            currentTree = currentTree.getLeftChild()
+        elif i not in ['+', '-', '*', '/', ')']:
+            currentTree.setRootVal(int(i))
+            currentTree = pStack.pop()
+        elif i in ['+','-','*','/']:
+            currentTree.setRootVal(i)
+            currentTree.insertRight('')
+            pStack.push(currentTree)
+            currentTree = currentTree.getRightChild()
+        elif i == ')':
+            currentTree = pStack.pop()
+        else:
+            raise ValueError
+    return eTree
+pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+pt.postorder()
+
+
+def evaluate(parseTree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC),evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+
+
+
